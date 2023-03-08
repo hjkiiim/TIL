a4beed42624fcb6e10de24ba60ba617e1c34d219
hjkiiim <mollyty0808@gmail.com>
2023-03-08 17:37:15 +0900

    Flyweight

---
+++ src/main/java/org/example/structuralPattern/flyweight/forest/Forest.java
@@ -0,0 +1,26 @@
+package org.example.structuralPattern.flyweight.forest;
+
+import org.example.structuralPattern.flyweight.tree.Tree;
+import org.example.structuralPattern.flyweight.tree.TreeFactory;
+import org.example.structuralPattern.flyweight.tree.TreeType;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Forest extends JFrame {
+    private List<Tree> trees = new ArrayList<>();
+    public void plantTree(int x, int y, String name, Color color, String otherTreeData){
+        TreeType type = TreeFactory.getTreeType(name, color, otherTreeData);
+        Tree tree = new Tree(x, y, type);
+        trees.add(tree);
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        for (Tree tree: trees){
+            tree.draw(graphics);
+        }
+    }
+}
