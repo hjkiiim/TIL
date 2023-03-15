3d555c0904f4be4a2284acc546ca9f05624fdaf9
hjkiiim <mollyty0808@gmail.com>
2023-03-15 12:45:13 +0900

    TemplateMethod

---
+++ src/main/java/org/example/behavioralPattern/templateMethod/Main.java
@@ -0,0 +1,34 @@
+package org.example.behavioralPattern.templateMethod;
+
+import org.example.behavioralPattern.templateMethod.network.Facebook;
+import org.example.behavioralPattern.templateMethod.network.Network;
+import org.example.behavioralPattern.templateMethod.network.Twitter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Network network = null;
+        System.out.print("Input user name : ");
+        String userName = reader.readLine();
+        System.out.print("Input password : ");
+        String password = reader.readLine();
+
+        System.out.print("Input message : ");
+        String message = reader.readLine();
+
+        System.out.println("\nChoose soccial network for posting message.\n"
+                            + "1. Facebook\n" + "2. Twitter");
+        int choice = Integer.parseInt(reader.readLine());
+
+        if(choice == 1){
+            network = new Facebook(userName, password);
+        } else if(choice == 2){
+            network = new Twitter(userName, password);
+        }
+        network.post(message);
+    }
+}
